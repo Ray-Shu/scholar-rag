1d20e26541786ea0321c07489097fb3deaafe179
Ray Shu <rayshu1015@gmail.com>
2026-08-07 17:26:58 -0600

    setup

---
+++ src/scholar-rag/main.py
@@ -0,0 +1,18 @@
+import os 
+from dotenv import load_dotenv 
+from google import genai 
+
+
+
+if __name__ == "__main__": 
+    load_dotenv() # loads env vars 
+
+    # client = genai.Client()
+
+    # interaction = client.interactions.create(
+    #     model="gemini-3.6-flash",
+    #     input="Explain how AI works in a few words"
+    # )
+    # print(interaction.output_text)
+
+
